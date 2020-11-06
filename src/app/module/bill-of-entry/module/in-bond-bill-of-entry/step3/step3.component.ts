1037692da36d76c180279ef6244773697a982ec2
Mithlesh kumar <aditya20apr@gmail.com>
2020-11-06 20:48:24 +0530

    code update

--- src/app/module/bill-of-entry/module/in-bond-bill-of-entry/step3/step3.component.ts
+++ src/app/module/bill-of-entry/module/in-bond-bill-of-entry/step3/step3.component.ts
@@ -399,7 +399,7 @@ export class Step3Component implements OnInit,ControlValueAccessor,Validator {
 //    console.log()
 //const expvalidation = <FormArray>this.addStep3Inoices().at(0).get('addNewItemStepThree');
 
-    return (
+    return (   
 
       (!this.addStep3Inoices().at(0).get(field)?.valid && this.addStep3Inoices().at(0).get(field)?.touched) ||
       (this.addStep3Inoices().at(0).get(field)?.untouched  && this.formSumitAttempt)
